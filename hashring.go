7d7996de225550b1b21a85b84b024c74ee078186
Devin Carlen <devcamcar@illian.local>
2010-06-29 19:42:49 -0700

    Implemented md5 hash key generation

---
+++ hashring.go
@@ -0,0 +1,21 @@
+//package hashring
+package main
+
+import (
+    "fmt";
+    "crypto/md5";
+    "hash";
+)
+
+// Creates an md5 based int value for the given string.
+func generateKey(key string) []uint8 {
+    var h hash.Hash = md5.New();
+    h.Write([]byte(key)); 
+    return h.Sum();
+}
+
+func main() {
+    key := "key_name";
+    hex := generateKey(key);
+    fmt.Printf("%s: %x\n", key, hex)    
+}
