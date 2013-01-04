c94343d9f00d12e8bfc1146790ede43146163217
Devin Carlen <devin.carlen@gmail.com>
2013-01-04 11:02:51 -0800

    Updated for golang 1.0

--- hashring.go
+++ hashring.go
@@ -2,20 +2,20 @@
 package main
 
 import (
-    "fmt";
-    "crypto/md5";
-    "hash";
+	"crypto/md5"
+	"fmt"
+	"hash"
 )
 
 // Creates an md5 based int value for the given string.
 func generateKey(key string) []uint8 {
-    var h hash.Hash = md5.New();
-    h.Write([]byte(key));
-    return h.Sum(nil);
+	var h hash.Hash = md5.New()
+	h.Write([]byte(key))
+	return h.Sum(nil)
 }
 
 func main() {
-    key := "key_name";
-    hex := generateKey(key);
-    fmt.Printf("%s: %x\n", key, hex)
+	key := "key_name"
+	hex := generateKey(key)
+	fmt.Printf("%s: %x\n", key, hex)
 }
